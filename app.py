0d7814ae95953e5f0731346d83afad1096b92b0e
Bill <xiaobill8@gmail.com>
2022-07-27 18:40:06 +0800

    Added scores APIs

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import csv
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/students/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/scores/*": {"origins": "*"}})
 cors = CORS(app, resources={r"/top-students": {"origins": "*"}})
 
 logging.getLogger("flask_cors").level = logging.DEBUG
@@ -19,6 +20,23 @@ def students():
         conn = connect()
         cur = conn.cursor(pymysql.cursors.DictCursor)
         cur.execute("SELECT * FROM student")
+        rows = cur.fetchall()
+        resp = jsonify(rows)
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
+
+
+@app.route("/scores", methods=["GET"])
+def students():
+    try:
+        conn = connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        cur.execute("SELECT * FROM score")
         rows = cur.fetchall()
         resp = jsonify(rows)
         resp.status_code = 200
@@ -59,30 +77,64 @@ def top_students():
         conn.close()
 
 
-# INSERT
+# Insert Student
 @app.route("/students/create-student", methods=["POST"])
 def inst():
-    conn = connect()
-    cur = conn.cursor(pymysql.cursors.DictCursor)
-    name = request.json["name"]
-    password = request.json["password"]
-    classId = request.json["class"]
-    query = f"insert into student (Name, Password, Class) values ('{name}', '{password}', '{classId}')"
-    cur.execute(query)
-    conn.commit()
-    cur.close()
-    output = {
-        "name": name,
-        "password": password,
-        "class": classId,
-        "Message": "Success",
-    }
-    resp = jsonify({"result": output})
+    try:
+        conn = connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        name = request.json["name"]
+        password = request.json["password"]
+        classId = request.json["class"]
+        query = f"insert into student (Name, Password, Class) values ('{name}', '{password}', '{classId}')"
+        cur.execute(query)
+        conn.commit()
+        cur.close()
+        output = {
+            "name": name,
+            "password": password,
+            "class": classId,
+            "Message": "Success",
+        }
+        resp = jsonify({"result": output})
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
 
-    return resp
+
+# Add Score
+@app.route("/scores/add-score", methods=["POST"])
+def inst():
+    try:
+        conn = connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        name = request.json["name"]
+        subject = request.json["subject"]
+        score = request.json["score"]
+        query = f"insert into score (Name, Subject, Score) values ('{name}', '{subject}', '{score}')"
+        cur.execute(query)
+        conn.commit()
+        cur.close()
+        output = {
+            "name": name,
+            "subject": subject,
+            "score": score,
+            "Message": "Success",
+        }
+        resp = jsonify({"result": output})
+
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
 
 
-# Upload CSV
+# Upload students CSV
 @app.route("/students/upload-csv", methods=["POST"])
 def upload():
     try:
@@ -104,13 +156,43 @@ def upload():
         resp.status_code = 200
         return resp
     except Exception as e:
+        # TODO: Return error resp to FE
         print(e)
     finally:
         cur.close()
         conn.close()
 
 
-# GET ONE
+# Upload scores CSV
+@app.route("/scores/upload-csv", methods=["POST"])
+def upload():
+    try:
+        conn = connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        form_dict = request.form.to_dict()
+        str_file_value = form_dict.get("csvInput")
+        file_t = str_file_value.splitlines()
+        csv_reader = csv.reader(file_t, delimiter=",")
+        next(csv_reader, None)  # skip the headers
+        for row in csv_reader:
+            query = f"insert into score (Name, Subject, Score) values ('{row[0]}', '{row[1]}', '{row[2]}')"
+            cur.execute(query)
+            conn.commit()
+        output = {
+            "Message": "Success",
+        }
+        resp = jsonify({"result": output})
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        # TODO: Return error resp to FE
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
+
+
+# Get one student
 @app.route("/students/update-student/<name>", methods=["GET"])
 def userone(name):
     try:
@@ -128,26 +210,79 @@ def userone(name):
         conn.close()
 
 
+# Get one score
+@app.route("/scores/update-score/<name>/<subject>", methods=["GET"])
+def userone(name, subject):
+    try:
+        conn = connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        cur.execute(
+            f"SELECT * FROM score WHERE Name = '{name}' AND Subject = '{subject}'"
+        )
+        rows = cur.fetchall()
+        resp = jsonify(rows)
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
+
+
 # UPDATE
 @app.route("/students/update-student/<name>", methods=["PUT"])
 def updates(name):
-    conn = connect()
-    cur = conn.cursor(pymysql.cursors.DictCursor)
-    password = request.json["password"]
-    classId = request.json["class"]
-    query = f"UPDATE student SET Password = '{password}', Class = '{classId}' WHERE Name = '{name}'"
-    cur.execute(query)
-    conn.commit()
-    cur.close()
-    output = {
-        "name": name,
-        "password": password,
-        "class": classId,
-        "Message": "Success",
-    }
-    resp = jsonify({"result": output})
-    resp.status_code = 200
-    return resp
+    try:
+        conn = connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        password = request.json["password"]
+        classId = request.json["class"]
+        query = f"UPDATE student SET Password = '{password}', Class = '{classId}' WHERE Name = '{name}'"
+        cur.execute(query)
+        conn.commit()
+        cur.close()
+        output = {
+            "name": name,
+            "password": password,
+            "class": classId,
+            "Message": "Success",
+        }
+        resp = jsonify({"result": output})
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
+
+
+# UPDATE
+@app.route("/scores/update-score/<name>/<subject>", methods=["PUT"])
+def updates(name, subject):
+    try:
+        conn = connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        score = request.json["score"]
+        query = f"UPDATE student SET Score = '{score}' WHERE Name = '{name}' AND Subject = '{subject}'"
+        cur.execute(query)
+        conn.commit()
+        cur.close()
+        output = {
+            "name": name,
+            "subject": subject,
+            "score": score,
+            "Message": "Success",
+        }
+        resp = jsonify({"result": output})
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
 
 
 # DELETE
@@ -157,6 +292,29 @@ def delete(name):
         conn = connect()
         cur = conn.cursor(pymysql.cursors.DictCursor)
         query = f"DELETE FROM student WHERE Name = '{name}'"
+        cur.execute(query)
+        conn.commit()
+        output = {
+            "name": name,
+            "Message": "DELETED",
+        }
+        resp = jsonify({"result": output})
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
+
+
+# DELETE
+@app.route("/scores/delete-score/<name>/<subject>", methods=["DELETE"])
+def delete(name, subject):
+    try:
+        conn = connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        query = f"DELETE FROM student WHERE Name = '{name}' AND Subject = '{subject}"
         cur.execute(query)
         conn.commit()
         output = {
